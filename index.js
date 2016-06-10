41de0b20b714e4eaa6525ca9fdfd5b14aa945cc7
Henrik Steen <steen.henke@gmail.com>
2016-06-10 14:11:54 +0200

    Make init better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { getFeed } = require('./lib/ecb-connect');
 const { parseXML } = require('./lib/parser');
 const { setCurrencies, convert } = require('./lib/converter');
+const packageInfo = require('./package.json');
 
 const updateCurrencies = () => {
     getFeed()
@@ -13,17 +14,13 @@ const updateCurrencies = () => {
         });
 };
 
-const init = () => {
-    console.log('Initializing RapidCurrency');
+// Initialize
+(() => {
+    console.log(`Initializing RapidCurrency v${packageInfo.version}`);
     updateCurrencies();
-};
+})();
 
 module.exports = {
-    init,
     convert,
     updateCurrencies,
 };
-
-init();
-
-setInterval(updateCurrencies, 1000);
